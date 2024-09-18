8bed74f2c4745aceb98ad5cc91b50a91d662164d
iFrames7 <ivanladino576@gmail.com>
2024-09-18 10:14:42 -0600

    First commit

---
+++ Source/MultiReal/MultiRealGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiRealGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMultiRealGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMultiRealGameMode();
+};
+
+
+
---
+++ Source/MultiReal/MultiRealPlayerController.cpp
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "MultiRealPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+void AMultiRealPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// get the enhanced input subsystem
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		// add the mapping context so we get controls
+		Subsystem->AddMappingContext(InputMappingContext, 0);
+
+		UE_LOG(LogTemp, Warning, TEXT("BeginPlay"));
+	}
+}
